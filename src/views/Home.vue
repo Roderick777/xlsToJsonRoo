193fe15b441449d4a4eb4c5ec904d4ac24f786b5
Roderick777 <38777569+Roderick777@users.noreply.github.com>
2020-05-27 18:53:06 -0400

    ajuste color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,9 +30,9 @@
             pre.uk-margin-top( v-if="vista == 'json'" :style="'font-size:'+ tamanoFuente +'px;'") {{ dataExcel | buscador( textoBuscar, campoFiltro)}}
             table( v-if="vista == 'html' && encabezadosTabla != null" :style="'font-size:'+ tamanoFuente +'px;'")
               tr
-                  th( v-for="(encabezado, i ) in encabezadosTabla", :key="'encabezado'+ i") {{ encabezado }}
+                th( v-for="(encabezado, i ) in encabezadosTabla", :key="'encabezado'+ i") {{ encabezado }}
               tr( v-for="(fila, i) in dataExcel", :key="'fila'+i")
-                  td(v-for="(celda, j) in valoresHojaFila(fila, i)") {{ celda }}
+                td(v-for="(celda, j) in valoresHojaFila(fila, i)") {{ celda }}
           div( v-else )
             div.alert.primary Aquí podrás visualizar la data en formato JSON una vez importar el excel, por defecto se selecciona la primera hoja del archivo excel, si deseas ver la información de otro hoja, solo has click en los botones de hoja que apareceran en la sección de importacion despues de seleccionar un archivo excel.
 </template>
@@ -42,45 +42,45 @@ import ImportarExcel from '../components/ImportarExcel';
 export default {
     components: { ImportarExcel },
     data() {
-        return {
-            rutaImportacion : '',
-            dataExcel       : null,
-            nombreHoja      : '',
-            tags            : [],
-            tag             : '',
-            datosEnviar     : null,
-            encabezadosTabla: null,
-            cargando        : false,
-            textoBuscar     : null,
-            campoFiltro     : null, 
-            tamanoFuente    : 10,
-            hojaActual      : null,
-            vista           : 'json'
-        }
+      return {
+        rutaImportacion   : '',
+        dataExcel         : null,
+        nombreHoja        : '',
+        tags              : [],
+        tag               : '',
+        datosEnviar       : null,
+        encabezadosTabla  : null,
+        cargando          : false,
+        textoBuscar       : null,
+        campoFiltro       : null, 
+        tamanoFuente      : 10,
+        hojaActual        : null,
+        vista             : 'json'
+      }
     },
     methods: {
-        excelProcesado(evento) {
-            console.log('respuesta excel', evento)
-            this.dataExcel = evento.datos
-            this.hojaActual = evento.hoja
-            setTimeout( ()=> {
-                this.encabezadosHoja(evento.datos)
-            }, 1000)
-        },
-        encabezadosHoja(dataExcel) {
-            this.encabezadosTabla = Object.keys(dataExcel[0])
-            console.log(Object.keys(dataExcel[0]))
-        },
-        valoresHojaFila(item) {
-            let valores = Object.values(item)
-            return valores;
-        },
-        elegirHoja(evento) { //EVENTO QUE  SE EJECUTA AL HACER CLICK SOBRE UN ELEMENTO "Hoja" DEL COMPONENTE "ImportarExcel"
-            this.nombreHoja = evento
-        },
-        cambiarVista(vista){
-          this.vista = vista
-        }
+      excelProcesado(evento) {
+        console.log('respuesta excel', evento)
+        this.dataExcel  = evento.datos
+        this.hojaActual = evento.hoja
+        setTimeout( () => {
+            this.encabezadosHoja(evento.datos)
+        }, 1000)
+      },
+      encabezadosHoja(dataExcel) {
+        this.encabezadosTabla = Object.keys(dataExcel[0])
+        console.log(Object.keys(dataExcel[0]))
+      },
+      valoresHojaFila(item) {
+        let valores = Object.values(item)
+        return valores;
+      },
+      elegirHoja(evento) { //EVENTO QUE  SE EJECUTA AL HACER CLICK SOBRE UN ELEMENTO "Hoja" DEL COMPONENTE "ImportarExcel"
+        this.nombreHoja = evento
+      },
+      cambiarVista(vista){
+        this.vista = vista
+      }
     },
     filters: {
       buscador(valor, textoBuscar, campoBuscar){
@@ -119,7 +119,7 @@ export default {
       position: fixed;
       top: 150px;
       right: 0px;
-      background-color: rgba($color-gray, .4);
+      background-color: rgba($color-gray, 1);
       padding: 0px 10px 10px 10px;
       font-size: .7rem;
       .titulo__herramientas{
