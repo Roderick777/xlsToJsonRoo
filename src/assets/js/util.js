57a745751bb9fb273ee5b7534fd7d839bf4cc088
Roderick777 <38777569+Roderick777@users.noreply.github.com>
2020-05-27 16:04:48 -0400

    ajuste eslint

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -4,20 +4,24 @@ export default {
     // se sugiere que el excel tenga su información a partir desde la primera celda y primera columna para simplificar el uso de esta funcionalidad
     leerExcel(files, hoja){
         return new Promise((resolve, reject) => {
-            let c               = this;
-            let reader          = new FileReader();
-            let nombreArchivo   = files[0].name;
-            reader.onload   = (e) => {
-                let workbook                = XLSX.read( new Uint8Array(e.target.result) , {type: 'array'});
-                hoja                        = (hoja == undefined )? workbook.SheetNames[0] : hoja
-                console.log('hojas', hoja)
-                let worksheet               = workbook.Sheets[hoja];
-                resolve({ 
-                    data : XLSX.utils.sheet_to_json(worksheet),
-                    hojas: workbook.SheetNames,
-                });
-            };
-            reader.readAsArrayBuffer(files[0]) // LEER ARCHIVO
+            try{
+                let c               = this;
+                let reader          = new FileReader();
+                // let nombreArchivo   = files[0].name;
+                reader.onload   = (e) => {
+                    let workbook                = XLSX.read( new Uint8Array(e.target.result) , {type: 'array'});
+                    hoja                        = (hoja == undefined )? workbook.SheetNames[0] : hoja
+                    console.log('hojas', hoja)
+                    let worksheet               = workbook.Sheets[hoja];
+                    resolve({ 
+                        data : XLSX.utils.sheet_to_json(worksheet),
+                        hojas: workbook.SheetNames,
+                    });
+                };
+                reader.readAsArrayBuffer(files[0]) // LEER ARCHIVO
+            } catch (e) {
+                reject(e)
+            }
         });
     },
 }
