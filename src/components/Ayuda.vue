2844ed8cd98bfba9bd3bcb72118d018512997a01
Roderick777 <38777569+Roderick777@users.noreply.github.com>
2020-05-27 20:10:07 -0400

    subir cambios

---
+++ src/components/Ayuda.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    div
+        div Proximamente sección de ayuda!
+        //- div
+        //-     ul
+        //-         li
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            filtros: []
+        }
+    }
+}
+</script>
